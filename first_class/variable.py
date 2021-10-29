b4fa2b4106e338bae7a82335c298400b18d3317f
Mahmudul-Hasan-Nahid <mahmudulhasanofficialplus@gmail.com>
2021-10-29 18:02:29 +0600

    first practice variable,conditions

---
+++ first_class/variable.py
@@ -0,0 +1,12 @@
+int_var=100
+float_var=10.2
+bool_var=True
+boolean_var2=False
+string_firstvar="hello want to be programmer? "
+
+
+print(type(int_var))
+print(type(float_var))
+print(type(bool_var))
+print(type(boolean_var2))
+print(type(string_firstvar))
